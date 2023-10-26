5ff82ea80f6fc8d9aa8b0db839db661d20fda24b
Queras <142896487+Queras@users.noreply.github.com>
2023-10-26 15:02:42 +0200

    Add jump
    adding jump to character controller. needs fixing

--- Assets/PlayerControler.cs
+++ Assets/PlayerControler.cs
@@ -5,22 +5,42 @@ using UnityEngine;
 public class PlayerControler : MonoBehaviour
 {
     public float moveSpeed = 10.0f;
-    public float turnSpeed = 100.0f;
+    public float turnSpeed = 200.0f;
+    public float jumpSpeed = 10.0f;
+    private Rigidbody m_rigidbody;
+    private float turn;
+    private float move;
+    private bool jump;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, Vector3.down * 1f);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_rigidbody = GetComponent<Rigidbody>();
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        float turn = Input.GetAxis("Horizontal") * Time.deltaTime * turnSpeed;
-        float move = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-
+        turn = Input.GetAxis("Horizontal") * Time.deltaTime * turnSpeed;
+        move = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        jump = Input.GetKeyDown(KeyCode.Space);
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {   
+        
         transform.Rotate(0.0f, turn, 0.0f);
         transform.position = transform.position + transform.forward * move;
-        
+        if (!(m_rigidbody.velocity.y == 0)) return;
+        if (jump) m_rigidbody.velocity += Vector3.up * jumpSpeed;
+
+
+
+
+
     }
 }
